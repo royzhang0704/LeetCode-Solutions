6ec6648f9a19f984e2a771b98148147715e5ffd2
張志強 <161924422+royzhang0704@users.noreply.github.com>
2024-05-31 14:59:30 +0800

    Create 2942. Find Words Containing Character.py

---
+++ Solution/String/2942. Find Words Containing Character.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        result=[]
+        for i in range(len(words)):
+            if x in words[i]:
+                result.append(i)
+        return result
