42249d5c39bc4df37a7a52892e24c1760cbe60cd
張志強 <161924422+royzhang0704@users.noreply.github.com>
2024-06-21 17:07:54 +0800

    Update 1. Two Sum.py

--- Solution/Arrays & Hashing/1. Two Sum.py
+++ Solution/Arrays & Hashing/1. Two Sum.py
@@ -1,6 +1,10 @@
 """
 暴力法
-time complexity:O(n^2) 第一個元素比對n-1次 第二個元素比對n-2 total cost =(n-1)+(n-2)+(n-3)+......+1=(1+(n-1)*n-1)/2 =O(n^2)
+time complexity:O(n^2) 
+第一個元素比對n-1次 
+第二個元素比對n-2 
+total cost =(n-1)+(n-2)+(n-3)+......+1
+=(1+(n-1)*n-1)/2 =O(n^2)
 space complexity:O(1)
 """
 class Solution:
@@ -16,46 +20,43 @@ class Solution:
 
 
 """
-hash table 
-time complexitu:O(n) 每個元素都去檢查他的diff 是否存在 最後遍歷整個array 可以得到唯一解
-space complexity:O(n) 有n個元素則dictionary 共要最多要push len(nums)個進去  len(nums)=n
+哈希表法
+time complexity: O(n) 
+每個元素都檢查其 diff 是否存在
+最終遍歷整個 array 可以得到唯一解
+space complexity: O(n) 
+最多需要存儲 n 個元素在字典中
 """
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-     diff={}     #key:target-num  value:index of array
-     for index,value in enumerate(nums):     #用enumerate來得到array的index 跟對應的value
-        if diff.get(value,None)==None:     #若當前diff 字典裡沒有當前的key值
-            diff[target-value]=index     #把該元素的與target的diff當作字典的key 把對應的index 當作hash table 的value
-        else:
-            return [index,diff[value]]    #若當前的element 已經在hash table 代表先前有某個元素的與當前元素相加會相等 且字典value為該元素的index 
-                    #return 的第一個index 為當前元素的index diff[value] 為另個元素與當前元素相加為target的index
-
-
-
-"""
-two pointer
-time complexity:O(n) 只會遍歷整個array一次
-space complexity:O(1) 沒有額外的儲存空間
-"""
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_index=list(zip(nums,range(len(nums))))    #把每個元素都跟他的index 做zip  tuple 的format 為(num,index)
-        num_index.sort()#sort 會根據tuple 的第一個元素去做排序
-
-        #num_index是一個list 而每個list 的element都是一個tutle 
-
-        left=0
-        right=len(nums)-1
-
-        while left <right:
-            current_sum=num_index[left][0]+num_index[right][0] #tuple 的index 0 是num , index 1 是index 
-            if current_sum==target:
-                return [num_index[left][1],num_index[right][1]]
-            elif current_sum<target:
-                left+=1
+        diff = {}  # key: target - num, value: index of array
+        for index, value in enumerate(nums):  # 用 enumerate 來得到 array 的 index 和對應的 value
+            if value not in diff:  # 若當前 diff 字典裡沒有當前的 key 值
+                diff[target - value] = index  # 把該元素與 target 的 diff 當作字典的 key，把對應的 index 當作字典的 value
             else:
-                right-=1
+                return [diff[value], index]  # 若當前的 element 已在字典中，則返回相應的 index
 
+
+
+
+"""
+雙指針法
+time complexity: O(n) 只需遍歷整個 array 一次
+space complexity: O(1) 沒有額外的儲存空間
+"""
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_index = sorted((num, index) for index, num in enumerate(nums))  # 生成 (num, index) 的 tuple 並排序
+        left, right = 0, len(nums) - 1
+        
+        while left < right:
+            current_sum = num_index[left][0] + num_index[right][0]
+            if current_sum == target:
+                return [num_index[left][1], num_index[right][1]]
+            elif current_sum < target:
+                left += 1
+            else:
+                right -= 1
 
 
 
