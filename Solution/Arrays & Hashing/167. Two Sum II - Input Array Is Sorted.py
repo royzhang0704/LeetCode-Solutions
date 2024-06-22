37af9e73dbd24b0f62f45109a862405f66733125
張志強 <161924422+royzhang0704@users.noreply.github.com>
2024-06-22 23:09:29 +0800

    Create 167. Two Sum II - Input Array Is Sorted.py

---
+++ Solution/Arrays & Hashing/167. Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left=0
+        right=len(numbers)-1
+
+        while left < right:
+            current_sum=numbers[left]+numbers[right]
+            if current_sum==target:
+                return [left+1,right+1]
+            elif current_sum<target:
+                left+=1
+            else:
+                right-=1
+
+
+"""
+time complexity:O(n)
+space complexity:(1)
+"""
+            
