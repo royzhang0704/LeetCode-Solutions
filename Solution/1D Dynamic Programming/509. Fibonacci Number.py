e56fae82dd18cf69d8f177a29007beda2be36dfb
張志強 <161924422+royzhang0704@users.noreply.github.com>
2024-06-26 15:46:40 +0800

    509. Fibonacci Number.py

---
+++ Solution/1D Dynamic Programming/509. Fibonacci Number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n<=1:
+            return n
+        dp=[0]*(n+1)
+        dp[0]=0
+        dp[1]=1
+        for i in range(2,len(dp)):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
