023eb11471c13d2603ccc4a24bcb9b79cfbc90e3
張志強 <161924422+royzhang0704@users.noreply.github.com>
2024-07-12 16:58:19 +0800

    Update 347. Top K Frequent Elements.py

--- Solution/Arrays & Hashing/347. Top K Frequent Elements.py
+++ Solution/Arrays & Hashing/347. Top K Frequent Elements.py
@@ -1,21 +1,28 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        num_freq={}
-        for i in nums:
-            num_freq[i]=num_freq.get(i,0)+1
+        count={}# {element:frequency}
+        frequency=[[] for i in range(len(nums)+1)] #創建一個burket 
+        result=[] 
+        for num in nums:
+                count[num]=count.get(num,0)+1
+        for num,fre in count.items():
+            frequency[fre].append(num)
 
+        for i in range(len(nums),0,-1):
+            for element in frequency[i]:
+                result.append(element)
+                if len(result)==k:
+                    return result
+        return result
+        
 
-        pq=[]
-        for entry in num_freq.items():
-            heapq.heappush(pq,(entry[1],entry[0]))
-            if len(pq)>k:
-                heapq.heappop(pq)
-
-
-        result=[]
-
-        for i in range(k-1,-1,-1):
-            result.append(heapq.heappop(pq)[1])
-
-        return result[::-1]
-                
+"""
+Time complexity:時間為O(n) 先創建一個字典把每個元素都計算出他的頻率需要O(n)
+                每個元素掃一次之後再把字典裡的每個元素放進桶子裡面一樣需要O(n)時間
+                最後再從桶子的最後一個開始遍歷append k次之後return result 也是O(n)
+                所以複雜度是O(3n)=O(n) 
+Space complexity:創建了一個長度為n的桶子list  因爲頻率次數可能為1~n 共O(n)
+                之後再創建一個字典計算出每個元素出現的頻率字典的space 也是O(N) 
+                之後再把top k most frequency element append到result 裡面 k最多為n 也是O(n)
+                所以space：O(3n)=O(n)
+"""
