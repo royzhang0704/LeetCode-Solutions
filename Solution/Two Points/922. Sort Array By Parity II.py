5c3417554edcbb949ca6f3ca54d1286163ab5339
張志強 <161924422+royzhang0704@users.noreply.github.com>
2024-07-17 14:02:45 +0800

    Create 922. Sort Array By Parity II.py

---
+++ Solution/Two Points/922. Sort Array By Parity II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        even=0
+        odd=1
+        n=len(nums)
+        while odd<n and even<n:
+            while even<n and  nums[even]%2==0 :
+                even+=2
+            while  odd<n and nums[odd]%2!=0:
+                odd+=2
+            if even<n and odd<n:
+                nums[even],nums[odd]=nums[odd],nums[even]
+        return nums
