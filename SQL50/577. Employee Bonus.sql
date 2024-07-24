20c3c7d1831121324d4397a4641697d752122fc4
張志強 <161924422+royzhang0704@users.noreply.github.com>
2024-07-24 23:02:35 +0800

    Delete SQL50/577. Employee Bonus.sql

--- SQL50/577. Employee Bonus.sql
+++
@@ -1,9 +0,0 @@
-# Write your MySQL query statement below
-select name,ifnull(bonus,null) as bonus
-from
-Employee
-left join 
-Bonus
-on Employee.empId=Bonus.empId
-where 
-bonus<1000 or bonus is null;
