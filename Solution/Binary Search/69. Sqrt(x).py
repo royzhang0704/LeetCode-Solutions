47ffd0e3db828f5daa5efbfe3b996512227ad29f
張志強 <161924422+royzhang0704@users.noreply.github.com>
2024-07-27 01:03:30 +0800

    Create 69. Sqrt(x).py

---
+++ Solution/Binary Search/69. Sqrt(x).py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x<2:
+            return x
+        left=0
+        right=x//2
+        while left<=right:
+            mid=(right+left)//2
+            if mid**2==x:
+                return mid
+            elif mid**2<x:
+                left=mid+1
+            elif mid**2>x:
+                right=mid-1
+        return right
