c60c49f50e7c7d8145f132a1db8d1e32ab28a826
Roy <161924422+royzhang0704@users.noreply.github.com>
2024-09-15 23:08:33 +0800

    Update 2544. Alternating Digit Sum.py

--- Solution/Math/2544. Alternating Digit Sum.py
+++ Solution/Math/2544. Alternating Digit Sum.py
@@ -1,8 +1,10 @@
 class Solution:
     def alternateDigitSum(self, n: int) -> int:
+        if n<10:
+            return n
         ans,sign=0,1
         while n:
             ans+=n%10*sign
             sign=-sign
             n//=10
-        return ans
+        return ans*-sign
