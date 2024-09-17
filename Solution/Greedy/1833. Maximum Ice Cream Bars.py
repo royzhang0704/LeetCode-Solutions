25c07bba07527225812d1f709a3f45d2101900ce
Roy <161924422+royzhang0704@users.noreply.github.com>
2024-09-17 17:23:45 +0800

    Update 1833. Maximum Ice Cream Bars.py

--- Solution/Greedy/1833. Maximum Ice Cream Bars.py
+++ Solution/Greedy/1833. Maximum Ice Cream Bars.py
@@ -1,4 +1,8 @@
 class Solution:
+    """
+    time:O(nlogn):排序O(nlogn)+貪心 從最小價位開始拿
+    space:O(n) 排序產生得額外空間
+    """
     def maxIceCream(self, costs: List[int], coins: int) -> int:
         if sum(costs)<=coins:
             return len(costs)
